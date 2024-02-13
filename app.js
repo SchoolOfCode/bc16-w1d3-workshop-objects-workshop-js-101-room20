8f276e15c9ef2dddbe227e74b99f1c3fa359b62f
moose-hub <infamollagames@gmail.com>
2024-02-13 11:33:26 +0000

    getting to grips with objects

---
+++ app.js
@@ -0,0 +1,37 @@
+// What do objects look like?
+
+let myObject = {
+    Laptop1: {
+        screenSize: "17",
+        gpu: "2080",
+        year: "2024",
+    },
+    Laptop2: {
+        screenSize: "17",
+        gpu: "2080",
+        year: "2024",
+    }
+}
+
+myObject.make
+
+
+// How are objects useful
+
+let loginDetails = {
+    user: "",
+    password: "",
+    code: ""
+}
+
+
+
+// Object for each person
+
+let james = {
+    eyeColor: "green",
+    height: "5ft11",
+    age: "28",
+    favouriteFood: "Everything",
+    other: "has 3 chidlren"
+}
