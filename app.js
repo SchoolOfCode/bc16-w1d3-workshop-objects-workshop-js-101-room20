35e25dc55f952423ad12a433f39d034dfcf20330
moose-hub <infamollagames@gmail.com>
2024-02-13 11:58:38 +0000

    Creating nested objects and exploring complicated types within object

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let loginDetails = {
 let james = {
     eyeColor: "green",
     height: "5ft11",
-    age: "28",
+    age: 28,
     favouriteFood: "Everything",
     other: "has 3 chidlren"
 }
@@ -53,3 +53,38 @@ tomas.age = tomas.age + 1
 
 console.log(tomas.age)
 
+
+
+// - create an object that represents your team. Each team member should be represented as a nested object with the following properties (choose what you want for the values):
+//   - nickname
+//   - superpower
+//   - methodOfTransport
+//   - favouriteAnimal
+// - log the nested `favouriteAnimal` of each team member to the console
+
+
+let teamObject = {
+    james: {
+        nickname: "jimbo",
+        superPower: "Flying",
+        methodOfTransport: "Flying",
+        favouriteAnimal: "Wolf",
+        likesCats: false
+    },
+    tomas: {
+        nickname: "tom",
+        superPower: "invisibility",
+        methodOfTransport: "walking",
+        "favourite animal": "Sharks",
+        likesCats: false,
+        logFavouriteAnimal: function() {
+            console.log(teamObject.tomas["favourite animal"]);
+        }
+    }
+}
+
+console.log(teamObject.james.favouriteAnimal);
+
+console.log(teamObject.james.likesCats);
+
+teamObject.tomas.logFavouriteAnimal();
